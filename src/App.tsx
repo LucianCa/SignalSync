d6c27da4b9ea0e1a2a39a8d82ca194a5a3b3e883
LucianCa <uhaidvoz814@hotmail.com>
2025-04-13 14:27:43 +0800

    initial project setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>SignalSync</h1>
+      <p>Crypto Trading Signals Platform</p>
+    </div>
+  );
+}
+
+export default App;
